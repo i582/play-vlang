952fe69dd74c8e58baaaebfb8834d054bb952d8b
i582 <51853996+i582@users.noreply.github.com>
2022-10-23 04:07:16 +0400

    update
    - fixed styles
    - added examples
    - added handling errors for run and format
    - fixed string interpolation

--- www/js/examples.js
+++ www/js/examples.js
@@ -8,15 +8,19 @@ println('Hello, world!')
     {
         name: "Fibonacci",
         code: `
-fn fib(a int, b int) {
-    val := a + b
-    println(val)
-    if val < 1000 {
-        fib(b, val)
+fn fib(n int) int {
+    mut f := []int{len: n + 2}
+    f[0] = 0
+    f[1] = 1
+    for i := 2; i <= n; i++ {
+        f[i] = f[i - 1] + f[i - 2]
     }
+    return f[n]
 }
 
-fib(0, 1)
+for i in 0 .. 30 {
+    println(fib(i))
+}
 `
     },
     {
@@ -27,8 +31,162 @@ for area in areas {
     println('Hello, $area developers!')
 }
 `
+    },
+    {
+        name: "HTTP + time",
+        code: `
+import time
+import net.http
+
+resp := http.get('https://vlang.io/utc_now') or {
+    println('failed to fetch data from the server')
+    return
+}
+
+t := time.unix(resp.text.int())
+println(t.format()) // 2022-10-22 23:19
+`
+    },
+    {
+        name: "JSON Encoding/Decoding",
+        code: `
+import json
+
+struct User {
+    name string
+    age  int
+mut:
+    is_registered bool
+}
+
+fn main() {
+    s := '[{"name":"Frodo", "age":25}, {"name":"Bobby", "age":10}]'
+    mut users := json.decode([]User, s) or {
+        eprintln('Failed to parse json')
+        return
+    }
+    for user in users {
+        println('$user.name: $user.age')
+    }
+    println('')
+    for i, mut user in users {
+        println('$i) $user.name')
+        if !user.can_register() {
+            println('Cannot register $user.name, they are too young')
+            continue
+        }
+
+        // \`user\` is declared as \`mut\` in the for loop,
+        // modifying it will modify the array
+        user.register()
+    }
+
+    // Let's encode users again just for fun
+    println('')
+    println(json.encode(users))
+}
+
+fn (u User) can_register() bool {
+    return u.age >= 16
+}
+
+fn (mut u User) register() {
+    u.is_registered = true
+}
+`
+    },
+    {
+        name: "Filter Log file",
+        code: `
+// Print file lines that start with "DEBUG:"
+import os
+
+// \`write_file\` returns a result (\`!\`), it must be checked
+os.write_file('app.log', '
+ERROR: log file not found
+DEBUG: create new file
+DEBUG: write text to log file
+ERROR: file not writeble
+') or {
+    // \`err\` is a special variable that contains the error
+    // in \`or {}\` blocks
+    eprintln('failed to read the file: $err')
+    return
+}
+
+// \`read_file\` returns a result (\`!string\`), it must be checked
+text := os.read_file('app.log') or {
+    eprintln('failed to read the file: $err')
+    return
+}
+
+lines := text.split_into_lines()
+for line in lines {
+    if line.starts_with('DEBUG:') {
+        println(line)
+    }
+}
+
+// DEBUG: create new file
+// DEBUG: write text to log file
+`
+    },
+    {
+        name: "Compiletime Reflection",
+        code: `
+struct User {
+    name string
+    age  int
+}
+
+fn main() {
+    data := 'name=Alice\\nage=18'
+    user := decode&lt;User>(data)
+    println(user)
+}
+
+fn decode&lt;T>(data string) T {
+    mut result := T{}
+    // compile-time \`for\` loop
+    // T.fields gives an array of a field metadata type
+    $for field in T.fields {
+        $if field.typ is string {
+            // $(string_expr) produces an identifier
+            result.$(field.name) = get_string(data, field.name)
+        } $else $if field.typ is int {
+            result.$(field.name) = get_int(data, field.name)
+        }
+    }
+    return result
+}
+
+fn get_string(data string, field_name string) string {
+    for line in data.split_into_lines() {
+        key_val := line.split('=')
+        if key_val[0] == field_name {
+            return key_val[1]
+        }
+    }
+    return ''
+}
+
+fn get_int(data string, field string) int {
+    return get_string(data, field).int()
+}
+
+// \`decode&lt;User>\` generates:
+// fn decode_User(data string) User {
+//     mut result := User{}
+//     result.name = get_string(data, 'name')
+//     result.age = get_int(data, 'age')
+//     return result
+// }
+`
     }
 ].map((example) => {
-    example.code = example.code.replaceAll("    ", "\t").trim();
+    example.code = example.code
+        .replaceAll("    ", "    ")
+        .replaceAll("&lt;", "<")
+        .trim();
     return example;
 })
