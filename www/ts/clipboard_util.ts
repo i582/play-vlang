2f868c535842c8fa3b34d831edbebccb3f21adcc
i582 <51853996+i582@users.noreply.github.com>
2022-10-24 23:41:57 +0400

    small fixes

--- www/ts/clipboard_util.ts
+++ www/ts/clipboard_util.ts
@@ -31,7 +31,7 @@ function copyTextToClipboard(text: string): void {
     navigator.clipboard.writeText(text).then(function () {
         console.log('Async: Copying to clipboard was successful!');
     }, function (err) {
-        console.error('Async: Could not copy text: ', err, 'fallback to old method');
         fallbackCopyTextToClipboard(text);
+        console.error('Async: Could not copy text: ', err, 'fallback to old method');
     });
 }
